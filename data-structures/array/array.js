5fd0275977731e5c9db9af4c298bcecefb88577c
iliyaML <mohamad.i.mohamad.lokman@vanderbilt.edu>
2021-12-04 23:41:42 +0800

    Added array data structure

---
+++ data-structures/array/array.js
@@ -0,0 +1,31 @@
+const arr = [1, 2, 3, 4, 5];
+
+// get size of array
+arr.length;
+
+// add item to the end of the array
+arr.push(1);
+
+// remove item at the end of the array and returns it
+arr.pop();
+
+// add item to the beginning of the array
+arr.unshift(1);
+
+// remove item at the beginning of the array and returns it
+arr.shift();
+
+// remove an item at a particular index
+// first parameter: index
+// second parameter: number of items to remove
+arr.splice(0, 1);
+arr.split();
+
+// get item at a particular index
+arr[10];
+
+// get last item in the array
+arr[arr.length - 1];
+
+// returns a string version of the array
+arr.toString();
