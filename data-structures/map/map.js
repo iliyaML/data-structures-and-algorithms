aa6b446bf56cad2e591c006335021d18bb01246f
iliyaML <mohamad.i.mohamad.lokman@vanderbilt.edu>
2021-12-04 23:42:30 +0800

    Added map data structure

---
+++ data-structures/map/map.js
@@ -0,0 +1,16 @@
+const myMap = new Map();
+
+// get size of map
+myMap.size;
+
+// add a key to the map
+myMapp.set("a", 1);
+
+// get the value of a key
+myMap.get("a");
+
+// check if key exists
+myMap.has("a");
+
+// delete a key from the map
+myMap.delete("a");
