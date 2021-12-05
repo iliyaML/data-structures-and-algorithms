b8c3018a5e58e710afcb20c2bf1b44526bac08c4
iliyaML <mohamad.i.mohamad.lokman@vanderbilt.edu>
2021-12-05 23:02:36 +0800

    Added set data structure

---
+++ data-structures/set/set.js
@@ -0,0 +1,16 @@
+const mySet = new Set();
+
+// get size of set
+mySet.size;
+
+// add item to the set
+mySet.add(321);
+
+// check if item exists in the set
+mySet.has(999);
+
+// remove item from the set
+mySet.delete(123);
+
+// remove all items in the set
+mySet.clear();
