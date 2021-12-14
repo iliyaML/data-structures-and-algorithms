b311d08f0f2c57c9fde8e899ce25f8f2f09af3da
iliyaML <mohamad.i.mohamad.lokman@vanderbilt.edu>
2021-12-14 23:05:06 +0800

    Added binary tree data structure

---
+++ data-structures/tree/binary-tree.js
@@ -0,0 +1,11 @@
+// node has properties:
+// val
+// left (points to left subtree if any)
+// right (points to right subtree if any)
+class BinaryTreNode {
+  constructor(val) {
+    this.val = val;
+    this.left = null;
+    this.right = null;
+  }
+}
