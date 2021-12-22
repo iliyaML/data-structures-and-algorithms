d59c570153af971f977aa626fb8cebb94169c8bf
iliyaML <mohamad.i.mohamad.lokman@vanderbilt.edu>
2021-12-22 17:29:05 +0800

    Modified binary search algorithm

--- algorithms/searching/binary-search/binary-search-iterative.js
+++ algorithms/searching/binary-search/binary-search-iterative.js
@@ -1,24 +1,37 @@
-// Assumes the input array is already sorted
-// Divide and Conquer
-// Time Complexity: O(log n)
-// Space Complexity: O(1)
+/**
+ * Assumes the input array is already sorted
+ * Divide and Conquer
+ * Time Complexity: O(log n)
+ * Space Complexity: O(1)
+ * @param {*} nums
+ * @param {*} target
+ * @param {*} start
+ * @param {*} end
+ * @returns
+ */
 const binarySearchIterative = (nums, target) => {
   let start = 0;
   let end = nums.length - 1;
 
   while (start <= end) {
     let mid = Math.floor((start + end) / 2);
+    // there are 3 cases we need to check
     if (nums[mid] === target) {
-      return true;
+      // evaluate at the mid point
+      return mid;
     } else if (nums[mid] < target) {
+      // squeeze the range, modify start
       start = mid + 1;
     } else {
+      // sequeeze the range, modify start
       end = mid - 1;
     }
   }
-  return false;
+
+  // if we haven't found anything, at this point the element isn't in the input array
+  return -1;
 };
 
 let nums = [1, 3, 5, 7, 8, 9];
-let target = 5;
-console.log(binarySearchIterative(nums, target));
+console.log(binarySearchIterative(nums, 100, 0, nums.length - 1)); // -1
+console.log(binarySearchIterative(nums, 9, 0, nums.length - 1)); // 5
--- algorithms/searching/binary-search/binary-search-recursive.js
+++ algorithms/searching/binary-search/binary-search-recursive.js
@@ -1,28 +1,38 @@
-// Assumes the input array is already sorted
-// Divide and Conquer
-// Time Complexity: O(log n)
-// Space Complexity: O(log n), recursive call stack
+/**
+ * Assumes the input array is already sorted
+ * Divide and Conquer
+ * Time Complexity: O(log n)
+ * Space Complexity: O(log n), recursive call stack
+ * @param {*} nums
+ * @param {*} target
+ * @param {*} start
+ * @param {*} end
+ * @returns
+ */
 const binarySearchRecursive = (nums, target, start, end) => {
   // base condition, return false (target not found)
   if (start > end) {
-    return false;
+    return -1;
   }
 
   // evaluate the element in the middle
   let mid = Math.floor((start + end) / 2);
-  if (nums[mid] === target) {
-    return true;
-  }
 
-  if (nums[mid] < target) {
+  // there are 3 cases we need to check
+  if (nums[mid] === target) {
+    // evaluate at the mid point
+    return mid;
+  } else if (nums[mid] < target) {
+    // squeeze the range, modify start
     start = mid + 1;
     return binarySearchRecursive(nums, target, start, end);
   } else {
+    // sequeeze the range, modify start
     end = mid - 1;
     return binarySearchRecursive(nums, target, start, end);
   }
 };
 
 let nums = [1, 3, 5, 7, 8, 9];
-let target = 100;
-console.log(binarySearchRecursive(nums, target, 0, nums.length - 1));
+console.log(binarySearchRecursive(nums, 100, 0, nums.length - 1)); // false
+console.log(binarySearchRecursive(nums, 9, 0, nums.length - 1)); // true
