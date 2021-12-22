45ef90458d7d0e4956ab883e310fc8cae2b0bbb2
iliyaML <mohamad.i.mohamad.lokman@vanderbilt.edu>
2021-12-22 17:34:47 +0800

    Modified linear search algorithm with comments

--- algorithms/searching/linear-search/linear-search.js
+++ algorithms/searching/linear-search/linear-search.js
@@ -1,9 +1,22 @@
-// Time Complexity: O(n), size of the input
+/**
+ * Time Complexity: O(n), size of the input
+ * Space Complexity: O(1)
+ * @param {*} arr
+ * @param {*} target
+ * @returns
+ */
 const linearSearch = (arr, target) => {
+  // loop through the entire array
   for (let i = 0, l = arr.length; i < l; ++i) {
+    // return true if target is found
     if (arr[i] === target) {
-      return true;
+      return i;
     }
   }
-  return false;
+  // if we've exited the loop, means we didn't find the target
+  return -1;
 };
+
+let nums = [1, 3, 5, 7, 8, 9];
+console.log(linearSearch(nums, 100)); // -1
+console.log(linearSearch(nums, 9)); // 5
