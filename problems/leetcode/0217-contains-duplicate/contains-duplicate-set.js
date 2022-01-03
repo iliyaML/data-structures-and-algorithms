b0a9f065d9bddbee0a706e48f237767b9ec0cdf3
iliyaML <mohamad.i.mohamad.lokman@vanderbilt.edu>
2022-01-03 22:02:58 +0800

    Added contain duplicate problem (solved using hash table)

---
+++ problems/leetcode/0217-contains-duplicate/contains-duplicate-set.js
@@ -0,0 +1,17 @@
+/**
+ * Time Complexity: O(n)
+ * Space Complexity: O(n)
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+const containsDuplicate = (nums) => {
+  const mySet = new Set();
+  for (let i = 0, l = nums.length; i < l; ++i) {
+    if (mySet.has(nums[i])) {
+      return true;
+    }
+    mySet.add(nums[i]);
+  }
+
+  return false;
+};
