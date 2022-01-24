def051384eea5da39aab252ba6d92bbcd9c90045
iliyaML <mohamad.i.mohamad.lokman@vanderbilt.edu>
2022-01-24 23:52:03 +0800

    Added structy fibonacci (iterative approach)

---
+++ problems/structy/vi-dynamic-programming/01-fibonacci/fib-iterative.js
@@ -0,0 +1,17 @@
+/**
+ * @param {*} n
+ * @returns
+ */
+ const fib = (n) => {
+    // base case
+    if (n === 0 || n === 1) {
+      return n;
+    }
+  
+    for(let ){
+
+    }
+  };
+  
+  console.log(fib(100));
+  
