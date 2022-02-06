93ebb80131b4d8d5b2a1216e4d065cc232d0165f
iliyaML <mohamad.i.mohamad.lokman@vanderbilt.edu>
2022-02-06 15:13:46 +0800

    Added maximum average subarray i problem (sliding window approach)

---
+++ problems/leetcode/0643-maximum-average-subarray-i/find-max-average-sliding-window.js
@@ -0,0 +1,31 @@
+/**
+ * Time Complexity: O(n)
+ * Space Complexity: O(1)
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+const findMaxAverage = (nums, k) => {
+  // initialize max average to the first element
+  // since there will be at least 1 element
+  let maxAverage = nums[0];
+  let sum = 0;
+
+  // sliding window algorithm
+  let start = 0;
+  for (let end = 0, l = nums.length; end < l; ++end) {
+    sum += nums[end];
+
+    if (end - start + 1 === k) {
+      let curAverage = sum / k;
+      maxAverage = Math.max(maxAverage, curAverage);
+
+      // move the window by deleting element at start from our sum
+      // and increment start
+      sum -= nums[start];
+      ++start;
+    }
+  }
+
+  return maxAverage;
+};
