b591466a0efba90e6ef91d56aafa576b1b323bb7
iliyaML <mohamad.i.mohamad.lokman@vanderbilt.edu>
2022-02-10 22:06:51 +0800

    Added string data structure

---
+++ data-structures/array/string.js
@@ -0,0 +1,27 @@
+let text = "Hello world";
+
+// length of text
+text.length;
+
+// character at index
+text.charAt(i);
+
+// code at index
+text.charCodeAt(i);
+
+// substring, 1st value is position, 2nd value is end position
+text.substring(1, s.length - 1);
+
+// substr, 1st value is position, 2nd value is length
+// some say this method is deprecated
+text.substr(1, 3);
+
+// normal for loop going through character by character
+for (let i = 0, l = text.length; i < l; ++i) {
+  console.log(i);
+}
+
+// loop through text
+for (let c of text) {
+  console.log(c);
+}
