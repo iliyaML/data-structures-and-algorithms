3effc4d02eb77d87f6d03f1405f2ae97c248bd0c
iliyaML <mohamad.i.mohamad.lokman@vanderbilt.edu>
2022-02-16 23:15:42 +0800

    Added same tree problem (bfs approach)

---
+++ problems/leetcode/0100-same-tree/is-same-tree-bfs.js
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {boolean}
+ */
+const isSameTree = (p, q) => {
+  const queue = [p, q];
+  while (queue.length > 0) {
+    const first = queue.shift();
+    const second = queue.shift();
+
+    if (!first && !second) continue;
+    if (!first || !second || first.val !== second.val) return false;
+
+    queue.push(first.left);
+    queue.push(second.left);
+    queue.push(first.right);
+    queue.push(second.right);
+  }
+
+  return true;
+};
