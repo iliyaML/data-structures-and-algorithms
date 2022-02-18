a46d49e948157fe18226ddff666093bfbebfc914
iliyaML <mohamad.i.mohamad.lokman@vanderbilt.edu>
2022-02-18 22:33:33 +0800

    Added some basics regex

---
+++ algorithms/regex/regex.js
@@ -0,0 +1,23 @@
+/**
+ * https://regex101.com/
+ * good place to try out regular expressions
+ */
+
+/**
+ * Modifiers
+ * g, i, m
+ */
+
+/**
+ * \d
+ * \s
+ * \S
+ */
+
+/**
+ * [abc] find any character between the brackets
+ * [^abc] inverse of above
+ * [0-9] range
+ * [^0-9] inverse of above
+ * (x|y) find alternatives
+ */
