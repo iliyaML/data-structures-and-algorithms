ec775e8f4b35a000ce9edba8658c6aab95a3523c
iliyaML <mohamad.i.mohamad.lokman@vanderbilt.edu>
2022-03-10 22:10:14 +0800

    Added encode and decode strings problem

---
+++ problems/leetcode/0271-encode-and-decode-strings/encode-decode.py
@@ -0,0 +1,32 @@
+class Solution:
+    """
+    @param: strs: a list of strings
+    @return: encodes a list of strings to a single string.
+    """
+
+    def encode(self, strs):
+        # write your code here
+        result = ""
+        for string in strs:
+            result += str(len(string)) + "#" + string
+
+        return result
+
+    """
+    @param: str: A string
+    @return: dcodes a single string to a list of strings
+    """
+
+    def decode(self, str):
+        # write your code here
+        result = []
+        i = 0
+        while i < len(str):
+            j = i
+            while str[j] != "#":
+                j += 1
+            length = int(str[i:j].strip())
+            result.append(str[j+1:j+1+length])
+            i = j + 1 + length
+
+        return result
