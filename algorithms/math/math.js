3f80324aff4533c5bc63a1dd14a7fbc9ca302a6a
iliyaML <mohamad.i.mohamad.lokman@vanderbilt.edu>
2022-03-15 22:24:40 +0800

    Added common math methods

---
+++ algorithms/math/math.js
@@ -0,0 +1,21 @@
+let num1 = 100.123;
+let num2 = 200.234;
+
+// some popular math methods
+Math.max(num1, num2);
+
+Math.min(num1, num2);
+
+Math.ceil(num1);
+
+Math.floor(num1);
+
+Math.round(num1);
+
+Math.trunc(num1);
+
+// returns a number between 0 and 1 (exclusive)
+Math.random();
+Math.floor(Math.random() * 10); // returns a random integer from 0 to 9
+Math.floor(Math.random() * 11); // returns a random integer from 0 to 10
+Math.floor(Math.random() * 10) + 1; // returns a random integer from 1 to 10
