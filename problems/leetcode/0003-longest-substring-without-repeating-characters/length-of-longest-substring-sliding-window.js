436b9491f058945d793076a2bba8c41839b0d098
iliyaML <mohamad.i.mohamad.lokman@vanderbilt.edu>
2022-05-02 00:08:49 +0800

    Modified longest substring without repeating characters problem (sliding window approach)

--- problems/leetcode/0003-longest-substring-without-repeating-characters/length-of-longest-substring-sliding-window.js
+++ problems/leetcode/0003-longest-substring-without-repeating-characters/length-of-longest-substring-sliding-window.js
@@ -5,21 +5,19 @@
  * @return {number}
  */
 const lengthOfLongestSubstring = (s) => {
-  // set to store our characters
+  // set to store our unique substring
   const mySet = new Set();
 
-  let start = 0;
   let longest = 0;
+  let start = 0;
 
   // increase our window
   for (let end = 0, l = s.length; end < l; ++end) {
-    if (mySet.has(s[end])) {
-      // shrink our window by
-      // increasing start and remove items at the start
-      while (mySet.has(s[end])) {
-        mySet.delete(s[start]);
-        ++start;
-      }
+    // shrink our window by
+    // increasing start and remove items at the start
+    while (mySet.has(s[end])) {
+      mySet.delete(s[start]);
+      ++start;
     }
 
     // continue to add to the set
