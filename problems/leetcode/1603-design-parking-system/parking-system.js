6ec30f6dac08f205cfd9004d5d99ec3428c70a33
iliyaML <mohamad.i.mohamad.lokman@vanderbilt.edu>
2022-06-08 21:47:02 +0800

    Modified design parking system problem with details

--- problems/leetcode/1603-design-parking-system/parking-system.js
+++ problems/leetcode/1603-design-parking-system/parking-system.js
@@ -1,19 +1,26 @@
 class ParkingSystem {
   /**
+   * Time Complexity: O(1)
+   * Space Complexity: O(1)
    * @param {number} big
    * @param {number} medium
    * @param {number} small
    */
   constructor(big, medium, small) {
     // store the values of each car type in an array
+    // idx: car type
+    // val: frequency
     this.count = [big, medium, small];
   }
 
   /**
+   * Time Complexity: O(1)
+   * Space Complexity: O(1)
    * @param {number} carType
    * @return {boolean}
    */
   addCar(carType) {
+    // check if the frequency for that cartype is more than 0
     return this.count[carType - 1]-- > 0;
   }
 }
