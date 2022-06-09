b832eaa6b7c504aab173be2cd49d84736f41b4a9
iliyaML <mohamad.i.mohamad.lokman@vanderbilt.edu>
2022-06-09 22:39:43 +0800

    Added find median from data stream problem (naive approach)

---
+++ problems/leetcode/0295-find-median-from-data-stream/median-finder-naive.js
@@ -0,0 +1,38 @@
+class MedianFinder {
+  constructor() {
+    this.data = [];
+  }
+
+  /**
+   * Time Complexity: O(1)
+   * @param {number} num
+   * @return {void}
+   */
+  addNum(num) {
+    this.data.push(num);
+  }
+
+  /**
+   * Time Complexity: O(n), due to sorting
+   * @return {number}
+   */
+  findMedian() {
+    // sort the data
+    this.data.sort((a, b) => a - b);
+
+    const len = this.data.length;
+
+    // if we have total length that is even
+    // return the average of the middle indices
+    if (len % 2 === 0) {
+      return (
+        (this.data[Math.floor(len / 2)] + this.data[Math.floor(len / 2) - 1]) /
+        2
+      );
+    }
+
+    // if it's odd
+    // return the middle index
+    return this.data[Math.floor(len / 2)];
+  }
+}
