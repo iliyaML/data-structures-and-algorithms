e0af693b37d793f40cee085689dbbebd0660507c
iliyaML <mohamad.i.mohamad.lokman@vanderbilt.edu>
2022-06-14 23:15:16 +0800

    Added customer who visited but did not make any transactions problem (sql)

---
+++ problems/leetcode/1581-customer-who-visited-but-did-not-make-any-transactions/solution.sql
@@ -0,0 +1,17 @@
+-- Solution 1 
+SELECT customer_id, COUNT(visit_id) AS count_no_trans
+FROM Visits
+WHERE visit_id NOT IN 
+(
+    SELECT visit_id 
+    FROM Transactions
+)
+GROUP BY customer_id
+
+-- Solution 2
+SELECT customer_id, COUNT(Visits.visit_id) AS count_no_trans
+FROM Visits
+LEFT JOIN Transactions
+ON Visits.visit_id = Transactions.visit_id
+WHERE Transactions.visit_id IS NULL
+GROUP BY customer_id;
