d587d9bbd107506a7647feeb4c8f987a3cd2a4ae
iliyaML <mohamad.i.mohamad.lokman@vanderbilt.edu>
2022-06-30 23:22:52 +0800

    Updated sales analysis iii problem

--- problems/leetcode/1084-sales-analysis-iii/solution.sql
+++ problems/leetcode/1084-sales-analysis-iii/solution.sql
@@ -1,3 +1,4 @@
+-- Solution 1
 SELECT 
     P.product_id, 
     P.product_name
@@ -8,3 +9,18 @@ GROUP BY product_id
 HAVING 
     MIN(sale_date) >= '2019-01-01' AND 
     MAX(sale_date) <= '2019-03-31';
+
+-- Solution 2
+SELECT 
+    P.product_id,
+    P.product_name 
+FROM Product P
+WHERE P.product_id IN
+(
+    SELECT S.product_id
+    FROM Sales S
+    GROUP BY S.product_id
+    HAVING 
+        MIN(S.sale_date) >= '2019-01-01' AND 
+        MAX(S.sale_date) <= '2019-03-31'
+);
