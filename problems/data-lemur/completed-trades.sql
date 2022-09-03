7e4a35576594b0f184573b012fa94938dd9ec602
iliyaML <mohamad.i.mohamad.lokman@vanderbilt.edu>
2022-09-03 10:43:36 +0800

    Removed old folder

--- problems/data-lemur/completed-trades.sql
+++
@@ -1,8 +0,0 @@
-SELECT users.city, COUNT(*) AS total_orders
-FROM 
-  (SELECT * FROM trades WHERE status = 'Completed') trades,
-  users
-WHERE trades.user_id = users.user_id
-GROUP BY users.city
-ORDER BY total_orders DESC
-LIMIT 3;
--- problems/data-lemur/sql-page-with-no-likes.sql
+++
@@ -1,8 +0,0 @@
-SELECT DISTINCT pages.page_id
-FROM
-  pages
-WHERE pages.page_id NOT IN 
-(
-  SELECT page_id FROM page_likes
-)
-ORDER BY pages.page_id ASC;
