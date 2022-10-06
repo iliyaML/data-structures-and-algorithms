aa90725ec08145a6efd0f337992e54ca044d14bb
iliyaML <mohamad.i.mohamad.lokman@vanderbilt.edu>
2022-10-06 23:49:51 +0800

    Added highest number of products problem (sql)

---
+++ problems/datalemur/sql-highest-products.sql
@@ -0,0 +1,13 @@
+SELECT 
+  user_id, 
+  COUNT(DISTINCT product_id) AS product_num
+FROM 
+  user_transactions
+GROUP BY 
+  user_id
+HAVING 
+  SUM(spend) > 1000
+ORDER BY 
+  product_num DESC, 
+  SUM(spend) DESC
+LIMIT 3;
